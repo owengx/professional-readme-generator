5e405d1d1d03c363dcc488e83202ab6983b59b16
owengx <owengayle19@gmail.com>
2024-10-10 18:11:44 -0400

    input my first two questions and got them to display. Removed const questions from index.js

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project called?"
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Describe what your application will do"
+    }
+]) 
+// const questions = [
+//     {
+//         type: "input",
+//         name: "title",
+//         message: "What is your project called?"
+//     },
+// ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    // should call fs.writefile
 
+}
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    // call inquirer.prompt(...).then(...)
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license == "MIT" ) {
+  return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "Mozilla") {
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0";
+  } else if (license === "PERL"){
+    return "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
+  } 
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -11,7 +19,8 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+function generateMarkdown(data) { //similar to HTML function from mini project,
+  //except with a readme template instead of HTML
   return `# ${data.title}
 
 `;
